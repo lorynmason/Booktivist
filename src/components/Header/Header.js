936e94bfd8423f57f0abc58a05346cc3130e2096
Loryn Mason <masonloryn@gmail.com>
2019-01-03 09:56:23 -0700

    Set up files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="title-search"></div>
+      <img src='../../styles/images/2000px-Fist.svg.png'/>
+    </header>
+  )
+}
