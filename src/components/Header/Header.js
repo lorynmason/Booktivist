35c7199dfcff827d365c24185a7653f57eac68c7
Loryn Mason <masonloryn@gmail.com>
2019-01-03 10:09:23 -0700

    Setup Header Container

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,13 @@ import React from 'react';
 export const Header = () => {
   return (
     <header>
-      <div className="title-search"></div>
-      <img src='../../styles/images/2000px-Fist.svg.png'/>
+      <div className="title-search">
+        <h1>Booktivist</h1>
+        <h3>Read up, so you can Speak up</h3>
+        <input placeholder="Search for author or book"></input>
+      </div>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Fist.svg/2000px-Fist.svg.png'/>
+      {/* <img src='../../styles/images/2000px-Fist.svg.png'/> */}
     </header>
   )
 }
