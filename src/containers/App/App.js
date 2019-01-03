aeae7a346d70dedb584fc39b1269017923f62587
Loryn Mason <masonloryn@gmail.com>
2019-01-03 12:12:55 -0700

    Setup Header, move files

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header } from '../../components/Header/Header'
+import { Header } from '../../containers/Header/Header'
 import '../../styles/main.scss'
 
 class App extends Component {
