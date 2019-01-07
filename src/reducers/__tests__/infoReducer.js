ac66836286c23bba5536e84c84dda32322d3a77f
Loryn Mason <masonloryn@gmail.com>
2019-01-07 13:13:54 -0700

    Add info reducer test

---
+++ src/reducers/__tests__/infoReducer.js
@@ -0,0 +1,24 @@
+import { infoReducer } from '../infoReducer'
+
+describe('infoReducer', () => {
+  it('should return a default state', () => {
+    const expected = [];
+    const result = infoReducer([], {});
+
+    expect(result).toEqual(expected);
+  })
+
+  it('should return state with an info object', () => {
+    const mockInfo = {
+      name: "Into the Wild"
+    }
+    const mockAction = {
+      type: 'ADD_INFO',
+      info: mockInfo
+    };
+    const expected = mockInfo
+
+    const result = infoReducer({}, mockAction);
+    expect(result).toEqual(expected);
+  });
+})
--- src/reducers/infoReducer.js
+++ src/reducers/infoReducer.js
@@ -1,11 +1,7 @@
 export const infoReducer = (state={}, action) => {
   switch(action.type) {
     case 'ADD_INFO':
-      const info = action.info.reduce((obj, item) => {
-        obj= item
-        return obj
-      }, {})
-      return info
+      return action.info
     default:
       return state
   }
