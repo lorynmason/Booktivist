bc3f60f8b0ce312acd39d8db76ae9911dd003532
Loryn Mason <masonloryn@gmail.com>
2019-01-07 20:17:43 -0700

    Add test for Loader

---
+++ src/components/Loader/Loader.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Loader } from './Loader'
+import { shallow } from 'enzyme'
+
+describe('Loader', () => {
+  it('should match the snapshot', () => {
+    let wrapper = shallow(<Loader />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
